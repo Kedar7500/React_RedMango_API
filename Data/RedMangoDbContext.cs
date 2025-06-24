a3d002f9790cc45827b4570173d494fd54b990e9
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-24 16:21:37 +0530

    creation of dbcontext and applying migrations

---
+++ Data/RedMangoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Models;
+
+namespace RedMango_API.Data
+{
+    public class RedMangoDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public RedMangoDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure the dbcontext
+builder.Services.AddDbContext<RedMangoDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RedMangoDBConnectionString"));
+});
+
+// configure the identity
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<RedMangoDbContext>();
 
 var app = builder.Build();
 
