7efd679daa7db1fae4072a9d2e6cf2490783581a
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-25 10:59:13 +0530

    modifying menuitem dto

--- Models/DTO/MenuItemDto.cs
+++ Models/DTO/MenuItemDto.cs
@@ -4,21 +4,12 @@ namespace RedMango_API.Models.DTO
 {
     public class MenuItemDto
     {
-        [Required]
+        public int Id { get; set; }
         public string Name { get; set; }
-
-        [Required]
         public string Description { get; set; }
         public string SpecialTag { get; set; }
-
-        [Required]
         public string Category { get; set; }
-
-        [Required]
-        [Range(1,int.MaxValue)]
         public double Price { get; set; }
-
-        [Required]
         public string Image { get; set; }
     }
 }
