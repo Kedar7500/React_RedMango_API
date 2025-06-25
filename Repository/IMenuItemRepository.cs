c950e0b52f18b52482b407a5040b82a19a6e23f7
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-25 10:59:52 +0530

    Implementinmg repository pattern for MenuItems

---
+++ Repository/IMenuItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using RedMango_API.Models.Domain;
+
+namespace RedMango_API.Repository
+{
+    public interface IMenuItemRepository
+    {
+        Task<List<MenuItem>> GetMenuItems();
+    }
+}
---
+++ Repository/SQLMenuItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedMango_API.Data;
+using RedMango_API.Models.Domain;
+
+namespace RedMango_API.Repository
+{
+    public class SQLMenuItemRepository : IMenuItemRepository
+    {
+        private readonly RedMangoDbContext dbContext;
+
+        public SQLMenuItemRepository(RedMangoDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<MenuItem>> GetMenuItems()
+        {
+            return await dbContext.menuItems.ToListAsync();
+        }
+    }
+}
