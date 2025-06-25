f15236664ab2107d8b97df15d62a18afb244d29a
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-25 11:00:45 +0530

    configuring the Serilog, AutoMapper and DI of repository

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RedMango_API.Data;
+using RedMango_API.Mappings;
+using RedMango_API.Repository;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// configure the serilog 
+var logger = new LoggerConfiguration()
+             .WriteTo.Console()
+             .MinimumLevel.Information()
+             .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+             
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +32,13 @@ builder.Services.AddDbContext<RedMangoDbContext>(options =>
 
 // configure the identity
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<RedMangoDbContext>();
+
+// configure the DI
+builder.Services.AddScoped<IMenuItemRepository, SQLMenuItemRepository>();
+
+
+// configure the automapper
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 
